c071a9bd21da49f61d528cee6b698e64a6f0d62e
Fatih Gürsoy <fthgrsy@windowslive.com>
2022-06-21 15:59:45 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Fatih Gursoy on 21.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
